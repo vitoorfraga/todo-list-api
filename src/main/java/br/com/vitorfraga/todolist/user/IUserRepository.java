9b17c5999df5c82c4acc13bb7e069dab4fcd31fa
Vitor Fraga <vitorfragaps@gmail.com>
2023-10-12 12:18:20 -0300

    Create new User

---
+++ src/main/java/br/com/vitorfraga/todolist/user/IUserRepository.java
@@ -0,0 +1,14 @@
+package br.com.vitorfraga.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Interface é um modelo/contrato dentro da aplicação. Temos os métodos mas não temos as implementações dos métodos.
+ * Em uma interface só temos a representação dos métodos.
+ */
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+  
+}
--- src/main/java/br/com/vitorfraga/todolist/user/UserController.java
+++ src/main/java/br/com/vitorfraga/todolist/user/UserController.java
@@ -1,25 +1,23 @@
 package br.com.vitorfraga.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
- *  Modificadores: public | private | protected
- *  Tipo: class | interface | enum ....
- */
-
-
- @RestController
- @RequestMapping("/user")
+@RestController
+@RequestMapping("/user")
 
 public class UserController {
-  
+
+  @Autowired // => Define que o spring irá gerenciar todo o ciclo de vida do Repositório.
+  private IUserRepository userRepository;
 
   // => Método de cadastro:
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUserName());
+  public UserModel create(@RequestBody UserModel userModel) {
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 }
