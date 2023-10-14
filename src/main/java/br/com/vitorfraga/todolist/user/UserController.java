93d742b415daea681931ed907dc494e0def92c42
Vitor Fraga <vitorfragaps@gmail.com>
2023-10-14 15:17:22 -0300

    User encryption password

--- src/main/java/br/com/vitorfraga/todolist/user/UserController.java
+++ src/main/java/br/com/vitorfraga/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.vitorfraga.todolist.user;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +30,11 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existente");
     };
     
+    // => Criptograda a senha do usuário
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    
+    userModel.setPassword(passwordHashred);
+
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.ACCEPTED).body(userCreated);
   }
