d1fb841d52a93eef827a30bdcf68b7088c91253c
Vitor Fraga <vitorfragaps@gmail.com>
2023-10-15 16:47:44 -0300

    Validating owner user

--- src/main/java/br/com/vitorfraga/todolist/task/TaskControler.java
+++ src/main/java/br/com/vitorfraga/todolist/task/TaskControler.java
@@ -54,12 +54,25 @@ public class TaskControler {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
 
     var task = this.taskRepository.findById(id).orElse(null);
 
+    if(task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada.");
+    }
+
+    var idUser = request.getAttribute("idUser");
+
+    if(!task.getIdUser().equals(idUser)) {
+      System.out.println("CAIUUUU AQUI");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário invalído.");
+    }
+
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
+
+    return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
   }
 }
