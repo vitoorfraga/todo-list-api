1bb74c012506d71bfaadba66b5398b6a49dea04d
Vitor Fraga <vitorfragaps@gmail.com>
2023-10-15 17:23:46 -0300

    Error handler

--- src/main/java/br/com/vitorfraga/todolist/task/TaskModel.java
+++ src/main/java/br/com/vitorfraga/todolist/task/TaskModel.java
@@ -31,4 +31,12 @@ public class TaskModel {
   private LocalDateTime createdAt;
 
   private UUID idUser;
+
+  public void setTitle(String title) throws Exception{
+    if(title.length() > 50) {
+      throw new Exception("O campo title deve conter nom áximo 50 caracteres");
+    }
+    
+    this.title = title;
+  }
 }
